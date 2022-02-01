ee9ca141f0eaa29a11193f63544360a403c3bfd7
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-02-01 13:26:47 +0100

    Temporary Prisma debugger middleware

--- server/src/prisma.ts
+++ server/src/prisma.ts
@@ -2,6 +2,19 @@ import { PrismaClient } from "@prisma/client";
 import logger from "./utils/logger";
 
 const prismaClient = new PrismaClient();
+
+prismaClient.$use(async (params, next) => {
+  const before = Date.now();
+  const result = await next(params);
+  const after = Date.now();
+
+  logger.debug(
+    `Query ${params.model}.${params.action} took ${after - before}ms`
+  );
+
+  return result;
+});
+
 logger.info("Connected to Prisma Client");
 
 export default prismaClient;
