05b12b5a863a5a7e69ef88f73146e0aac31c0cf2
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-02-05 13:44:55 +0100

    MonthlyView Responsive

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -1,13 +1,13 @@
+import Button from "./Button";
+
 interface TileProps {
   date: string;
 }
 
 export default function Tile({ date }: TileProps) {
   return (
-    <button className=" w-28 h-28 bg-slate-50 rounded-md">
-      <h1 className=" -mt-12 ml-12 font-semibold text-gray-500 text-lg">
-        {date}
-      </h1>
-    </button>
+    <Button>
+      <h1 className="mb-16 m-1 font-semibold text-gray-500 text-lg">{date}</h1>
+    </Button>
   );
 }
--- client/src/modules/calendar/month/MonthView.tsx
+++ client/src/modules/calendar/month/MonthView.tsx
@@ -2,7 +2,7 @@ import Tile from "../../../components/Tile";
 
 export default function MonthView() {
   return (
-    <section className=" grid grid-cols-7 p-2 rounded-md bg-slate-200 gap-1">
+    <section className=" grid grid-cols-7 p-2 auto-cols-max auto-rows-max rounded-md bg-slate-200 gap-1 w-full h-full">
       <Tile date={1} />
       <Tile date={1} />
       <Tile date={1} />
