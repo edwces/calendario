64c76c061bf3de032f8f537db56cfac94b068750
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-02-07 20:06:50 +0100

    temporary responsive Calendar View

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -6,8 +6,8 @@ type TileProps = ButtonProps & {
 
 export default function Tile({ date, className, ...props }: TileProps) {
   return (
-    <Button className={` ${className}`} {...props}>
-      <h1 className="-mt-2 text-center font-semibold text-gray-500 text-lg">
+    <Button className={`grow ${className}`} {...props}>
+      <h1 className="relative bottom-1/3 text-center font-semibold text-gray-500 text-lg">
         {date}
       </h1>
     </Button>
--- client/src/modules/calendar/month/MonthView.tsx
+++ client/src/modules/calendar/month/MonthView.tsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
 import Tile from "../../../components/Tile";
 import EventController from "../EventController";
-import EventModal from "../EventModal";
 
 export default function MonthView() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className=" grid grid-cols-7 p-2 auto-cols-max auto-rows-max rounded-md bg-slate-200 gap-1 w-full h-full">
-      <Tile key={0} date={30}></Tile>
-      <Tile key={1} date={31}></Tile>
-      {[...Array(31)].map((item, index) => (
-        <Tile
-          key={index + 2}
-          date={index + 1}
-          onClick={() => setOpen(true)}
-        ></Tile>
+    <section className="flex flex-col p-2 h-full rounded-md bg-slate-200 gap-1">
+      {[...Array(5)].map((item, index) => (
+        <div className="grow flex flex-row gap-1" key={index}>
+          {[...Array(7)].map((item, index) => (
+            <Tile
+              key={index + 2}
+              date={index + 1}
+              onClick={() => setOpen(true)}
+            ></Tile>
+          ))}
+        </div>
       ))}
-      <Tile key={32} date={1}></Tile>
-      <Tile key={33} date={2}></Tile>
+
       <EventController isOpen={isOpen} onRequestClose={() => setOpen(false)} />
     </section>
   );
