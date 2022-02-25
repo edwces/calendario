ab4c1165ce3259b52bd05fd31f6ec06c75bf3156
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-02-25 19:28:21 +0100

    sidebar design for now

--- client/src/modules/sidebar/Sidebar.tsx
+++ client/src/modules/sidebar/Sidebar.tsx
@@ -2,11 +2,19 @@ import Checkbox from "../../components/Checkbox";
 
 export default function Sidebar() {
   return (
-    <aside className=" bg-slate-100 w-64">
-      <h1>My Calendars:</h1>
-      <Checkbox>Main</Checkbox>
-      <Checkbox>Second</Checkbox>
-      <Checkbox>Third</Checkbox>
+    <aside className=" bg-slate-50 w-72 p-2 pl-5">
+      <h1 className="font-extrabold text-4xl underline  decoration-pink-500 text-slate-700 font-mono">
+        CALENDARIO
+      </h1>
+      <h1 className="font-extrabold text-3xl text-gray-600 mt-10">Calendars</h1>
+      <div
+        className="ml-5
+      mt-2 flex flex-col gap-1"
+      >
+        <Checkbox>Main</Checkbox>
+        <Checkbox>Second</Checkbox>
+        <Checkbox>Third</Checkbox>
+      </div>
     </aside>
   );
 }
